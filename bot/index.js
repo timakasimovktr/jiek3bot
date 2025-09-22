885b34de75b0d82fe397ab15ce170cc2468c57b7
Timur Kasimov <timakasimovktr@gmail.com>
2025-09-22 19:56:15 +0500

    sdfghj

--- bot/index.js
+++ bot/index.js
@@ -312,7 +312,7 @@ bot.hears("📊 Navbat holati", async (ctx) => {
           month: "2-digit",
           year: "numeric",
         })}
-⌚️ Kelishi sana: ${new Date(latestBooking.created_at).toLocaleString("uz-UZ", {
+⌚️ Kelishi sana: ${new Date(latestBooking.start_datetime).toLocaleString("uz-UZ", {
           day: "2-digit",
           month: "2-digit",
           year: "numeric",
